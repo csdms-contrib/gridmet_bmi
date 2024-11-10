1fb1ff80e2619647ee3cfbaca83956f576c34cb8
Mark Piper <mark.piper@colorado.edu>
2024-11-09 17:00:04 -0700

    Include version in package

--- gridmet_bmi/__init__.py
+++ gridmet_bmi/__init__.py
@@ -1,7 +1,6 @@
 from .bmi_gridmet import BmiGridmet
-
-# from examples.bmi import BmiGridmet
 from .gridmet import Gridmet
 from .helpers import getaverage, np_get_wval
+from ._version import __version__
 
-__all__ = ["BmiGridmet", "Gridmet", "np_get_wval", "getaverage"]
+__all__ = ["BmiGridmet", "Gridmet", "np_get_wval", "getaverage", "__version__"]
